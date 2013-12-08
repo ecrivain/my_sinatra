27bf1943b44bbf67aca86ffe92717cd54cd574c0
ecrivain <jon@bowensnider.net>
2013-12-08 13:36:31 -0500

    SinatraRackapp

---
+++ rackmiddleware.rb
@@ -0,0 +1,18 @@
+class RackMiddleware
+
+	def initialize(appl)
+	end
+
+	def call(env)
+
+		start = Time.now
+		status, headers, body = @appl.call(env)
+
+		stop - Time.now
+
+		puts "Response Time: #{stop-start}"
+
+		{status, headers, body}
+	end
+
+end
